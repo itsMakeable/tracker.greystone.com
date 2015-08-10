7bb6b7e77dbed2576e183655dcfba0ecad1c9cf1
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 22:15:04 +1000

    Add /uploads directory to express.static.

--- server/server.js
+++ server/server.js
@@ -36,6 +36,7 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/../client', {
 	etag: false
 }));
+app.use('/uploads', express.static(__dirname + '/../uploads'));
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
