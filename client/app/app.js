e7879bd43edfdc1e3108891af953446171340901
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 21:00:39 +1000

    Add ng-file-upload dependency, config default routes and configure login redirect.

--- client/app/app.js
+++ client/app/app.js
@@ -6,6 +6,7 @@ import ngMessages from 'angular-messages';
 import angularToastr from 'angular-toastr';
 import jsData from 'js-data';
 import jsDataAngular from 'js-data-angular';
+require('../../node_modules/ng-file-upload/dist/ng-file-upload.js');
 require('../../node_modules/angular-toastr/dist/angular-toastr.css');
 
 var server = 'http://69.164.209.184:8080';
@@ -25,6 +26,7 @@ import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
+		'ngFileUpload',
 		jsDataAngular,
 		angularToastr,
 		ngMessages,
@@ -34,10 +36,27 @@ angular.module('app', [
 		Components.name,
 		Models.name
 	])
-	.config((DSProvider, DSHttpAdapterProvider, $httpProvider, $locationProvider) => {
+	.config((DSProvider, DSHttpAdapterProvider, $httpProvider, $urlRouterProvider) => {
+
+		if (window.localStorage.auth_token) {
+			$urlRouterProvider.otherwise('/homeScreen');
+		} else {
+			$urlRouterProvider.otherwise('/login');
+		}
 
 		DSProvider.defaults.basePath = local;
 		$httpProvider.useApplyAsync(true);
+
+	})
+	.run(($rootScope, User, $state) => {
+
+		$rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
+			console.log('$stateChangeError');
+			console.log(error);
+			if (error.notAuthenticated) {
+				$state.go('login');
+			}
+		});
 
 	})
 	.directive('app', AppComponent)
