5a8c2a597aaa94518ff9efcfeb02e5b761939efb
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 22:13:34 +1000

    Implement file upload route.

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,29 @@
 module.exports = function(app) {
 
+	var multer = require('multer');
+	var path = require('path');
+
+	var storage = multer.diskStorage({
+		destination: function(req, file, cb) {
+			cb(null, 'uploads/');
+		},
+		filename: function(req, file, cb) {
+			console.log('File');
+			var fileInformation = path.parse(file.originalname);
+			cb(null, fileInformation.name + '-' + new Date().getTime() + fileInformation.ext);
+		}
+	});
+
+	var upload = multer({
+		storage: storage
+	});
+
+	app.post('/api/upload', upload.array('file', 3), function(req, res, next) {
+		res.json(req.files);
+	});
+
 	require('./routes/field.js')(app);
+	require('./routes/file.js')(app);
 	require('./routes/message.js')(app);
 	require('./routes/milestone.js')(app);
 	require('./routes/project.js')(app);
