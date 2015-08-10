478385e0516e88dc8285e9b7fb32bc44a1efe130
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 21:01:42 +1000

    About component.

--- client/app/components/about/about.controller.js
+++ client/app/components/about/about.controller.js
@@ -1,12 +1,40 @@
 class AboutController {
-	constructor(User) {
+	constructor(User, $scope, Upload) {
 		this.User = User;
+		this.Upload = Upload;
+		console.log(Upload);
+		$scope.$watch(() => this.file, file => {
+			this.upload(this.file);
+		});
 	}
-	signup() {
-		this.User.signup();
+	upload(file) {
+		console.log(file);
+		if (file) {
+			this.Upload.upload({
+				url: 'http://localhost:8080/api/upload',
+				file: file
+			}).progress(evt => {
+				var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+				console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+			}).success((data, status, headers, config) => {
+				console.log(data);
+			}).error((data, status, headers, config) => {
+				console.log(status);
+			});
+		}
 	}
-	signin() {
-		this.User.signin();
+	uploadMulti() {
+		this.Upload.upload({
+			url: 'http://localhost:8080/api/upload',
+			file: this.files
+		}).progress(evt => {
+			var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+			console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+		}).success((data, status, headers, config) => {
+			console.log(data);
+		}).error((data, status, headers, config) => {
+			console.log(status);
+		});
 	}
 }
 
--- client/app/components/about/about.js
+++ client/app/components/about/about.js
@@ -1,17 +1,33 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import aboutComponent from './about.component'; 
+import template from './about.jade';
+import controller from './about.controller';
+import './about.styl';
 
 let aboutModule = angular.module('about', [
-	uiRouter
-])
-.config(($stateProvider)=>{
-	$stateProvider
-		.state('about', {
-			url: '/about',
-			template: '<about></about>'
-		});
-})
-.directive('about', aboutComponent);
+		uiRouter
+	])
+	.config(($stateProvider) => {
+		$stateProvider
+			.state('about', {
+				url: '/about',
+				template,
+				controller,
+				controllerAs: 'vm',
+				resolve: {
+					authentication: (User, $q) => {
+						return User.checkLogin()
+							.then(() => {
+								return true;
+							})
+							.catch(() => {
+								return $q.reject({
+									notAuthenticated: true
+								});
+							});
+					}
+				}
+			});
+	});
 
 export default aboutModule;
