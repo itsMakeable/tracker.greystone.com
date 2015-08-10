b1b72531fe55b98001a26cf226a6522f66b2dc54
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 22:10:17 +1000

    Import components.

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,10 +1,23 @@
 import angular from 'angular';
+
+import About from './about/about';
 import Home from './home/home';
-import About from './about/about'; 
+import HomeScreen from './homeScreen/homeScreen';
+import Login from './login/login';
+import MessagesView from './messagesView/messagesView';
+import ProjectView from './projectView/projectView';
+import SignUp from './signUp/signUp';
+import TaskView from './taskView/taskView';
 
 let componentModule = angular.module('app.components', [
+	About.name,
 	Home.name,
-	About.name
+	HomeScreen.name,
+	Login.name,
+	MessagesView.name,
+	ProjectView.name,
+	SignUp.name,
+	TaskView.name
 ]);
 
 export default componentModule;
