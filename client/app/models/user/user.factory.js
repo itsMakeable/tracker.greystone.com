8e9d84592e38169602d4a98a87bcc5829b892164
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 22:12:14 +1000

    Add signup, signing and checkLogin methods.

--- client/app/models/user/user.factory.js
+++ client/app/models/user/user.factory.js
@@ -1,4 +1,4 @@
-let UserFactory = function(DS, $http, LocalStorage) {
+let UserFactory = function(DS, $http, LocalStorage, $q) {
 
 	let userResource = DS.defineResource({
 		name: 'user',
@@ -6,32 +6,61 @@ let UserFactory = function(DS, $http, LocalStorage) {
 		endpoint: '/api/users',
 	});
 
+	let currentUser = null;
+
 	userResource.signup = function(user) {
-		return $http.post(DS.defaults.basePath + '/auth/signup', {
-				email: 'leandro.zubrezki@gmail.com',
-				password: '1234'
-			})
+		return $http.post(DS.defaults.basePath + '/auth/signup', user)
 			.then(response => {
 				console.log(response);
+				LocalStorage.set('auth_token', response.data.token);
+				currentUser = response.data.user;
+				return response.data;
 			})
 			.catch(response => {
 				console.log(response);
+				return $q.reject(response.data);
 			});
 	};
 
 	userResource.signin = function(email, password) {
 		return $http.post(DS.defaults.basePath + '/auth/signin', {
-				email: 'leandro.zubrezki@gmail.com',
-				password: '1234'
+				email,
+				password,
 			})
 			.then(response => {
 				console.log(response);
 				LocalStorage.set('auth_token', response.data.token);
+				currentUser = response.data.user;
+				return response.data;
 			})
 			.catch(response => {
 				console.log(response);
+				return $q.reject(response.data);
 			});
 	};
+
+	userResource.checkLogin = function() {
+		return $http.post(DS.defaults.basePath + '/auth/verify_token', {
+				token: LocalStorage.get('auth_token'),
+			})
+			.then(response => {
+				console.log(response);
+				if (!response.data.success) {
+					return $q.reject(response.data.message);
+				} else {
+					console.log('is logged in');
+					return response.data.decoded.user_id;
+				}
+
+			})
+			.catch(response => {
+				console.log(response);
+				console.log('is NOT logged in');
+				return $q.reject(response);
+			});
+	};
+
+	userResource.getCurrentUser = () => currentUser;
 
 	return userResource;
 
