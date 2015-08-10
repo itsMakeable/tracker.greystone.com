f02b07606b27489c2be6be4a82afa06a0d7191e8
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 22:12:53 +1000

    Component generator.

--- generator/component/temp.js
+++ generator/component/temp.js
@@ -1,10 +1,22 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import <%= name %>Component from './<%= name %>.component'; 
+import template from './<%= name %>.jade';
+import controller from './<%= name %>.controller';
+import './<%= name %>.styl';
 
-let <%= name %>Module = angular.module('<%= name %>', [
-	uiRouter
-])
-.directive('<%= name %>', <%= name %>Component);
+let <%= name %>
+Module = angular.module('<%= name %>', [
+		uiRouter
+	])
+	.config(($stateProvider, $urlRouterProvider) => {
 
-export default <%= name %>Module;
+		$stateProvider
+			.state('<%= name %>', {
+				url: '/<%= name %>',
+				template,
+				controller,
+				controllerAs: 'vm'
+			});
+	});
+export default <%= name %>
+Module;
