8127645478f300dab618ec13f2f5508e3f5beab4
leandroz <leandro.zubrezki@gmail.com>
2015-08-10 22:15:40 +1000

    TaskView state.

---
+++ client/app/components/taskView/taskView.controller.js
@@ -0,0 +1,50 @@
+class TaskViewController {
+	constructor(Project, $state, taskId, Task, $scope, Upload, File) {
+		this.Upload = Upload;
+		this.Project = Project;
+		this.project = Project.filter()[0];
+		this.task = Task.get(taskId);
+		this.File = File;
+		console.log(this.task);
+	}
+	fileDropped($files, $file, $event, $rejectedFiles, field) {
+		console.log('fileDropped');
+		console.log(this.field);
+		console.log($files);
+		console.log($file);
+		console.log($rejectedFiles);
+		if ($file) {
+			this.Upload.upload({
+				url: 'http://localhost:8080/api/upload',
+				file: $file
+			}).progress(evt => {
+				var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+				console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+			}).success((data, status, headers, config) => {
+				console.log(data[0].filename);
+				this.File.create({
+					name: data[0].filename,
+					field_id: field.field_id
+				});
+			}).error((data, status, headers, config) => {
+				console.log(status);
+
+			});
+		} else if ($files.length > 0) {
+			this.Upload.upload({
+				url: 'http://localhost:8080/api/upload',
+				file: $files
+			}).progress(evt => {
+				var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+				console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+			}).success((data, status, headers, config) => {
+				console.log(data);
+			}).error((data, status, headers, config) => {
+				console.log(status);
+			});
+		}
+	}
+}
+
+
+export default TaskViewController;
