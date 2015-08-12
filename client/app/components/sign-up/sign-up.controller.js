1c73884a073eaeef70f4f85be0dc01d88c641a13
leandroz <leandro.zubrezki@gmail.com>
2015-08-12 19:42:15 +1000

    Go to property view after sign up

--- client/app/components/sign-up/sign-up.controller.js
+++ client/app/components/sign-up/sign-up.controller.js
@@ -2,20 +2,16 @@ class SignUpController {
 	constructor(User, $state) {
 		this.User = User;
 		this.$state = $state;
-
-
 		this.userModel = {};
-
-
 	}
 	submit() {
 		if (this.signUpForm.$valid) {
 			this.User.signup(this.userModel)
 				.then(data => {
-
+					this.$state.go('property');
 				})
 				.catch(data => {
-
+					console.error(data);
 				});
 		} else {
 			this.signUpForm.$setSubmitted();
