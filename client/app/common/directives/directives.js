043748989ecad100d43ec4b56918403942a65c06
leandroz <leandro.zubrezki@gmail.com>
2015-08-15 19:05:39 +1000

    Import components.

--- client/app/common/directives/directives.js
+++ client/app/common/directives/directives.js
@@ -1,10 +1,16 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
-import UserProfileButton from './user-profile-button/user-profile-button';
+import CommentsBox from './comments-box/comments-box';
+import FieldRow from './field-row/field-row';
+import FileBox from './file-box/file-box';
+import SideBar from './side-bar/side-bar';
+import TaskRow from './task-row/task-row';
 
 let directivesModule = angular.module('directives.common', [
-	Navbar.name,
-	UserProfileButton.name,
+	CommentsBox.name,
+	FieldRow.name,
+	FileBox.name,
+	SideBar.name,
+	TaskRow.name
 ]);
 
 export default directivesModule;
