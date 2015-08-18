9b342441936e1480d4177ef13e00fc064509c64a
leandroz <leandro.zubrezki@gmail.com>
2015-08-18 15:18:13 +1000

    Add Authorization to header allowed

--- server/server.js
+++ server/server.js
@@ -42,7 +42,8 @@ app.use(express.static(__dirname + '/../client', {
 app.use('/uploads', express.static(__dirname + '/../uploads'));
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 app.use(morgan('dev'));
