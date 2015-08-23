29814e9d5b1ca6676a58741a43ac44a34b206738
leandroz <leandro.zubrezki@gmail.com>
2015-08-24 09:51:03 +1000

    Listen only in property for the socket event and then fire angular event from there. Order code and remove logs. View task before going to task view.

--- client/app/components/property/property.controller.js
+++ client/app/components/property/property.controller.js
@@ -1,51 +1,42 @@
 class PropertyController {
-	constructor(File, project, User, $scope, fromLogin) {
-		this.File = File;
+	constructor(project, User, $scope, fromLogin, socket, $rootScope) {
 		this.project = project;
 		this.fromLogin = fromLogin;
-		User.findAll({})
-			.then(users => {
-				console.log(users);
-			});
 
-		UPDATE_HEIGHT();
+		User.findAll({});
 
-		function UPDATE_HEIGHT() {
-			var elements = [];
-			var sideBar = document.getElementById('sideBar');
-			elements.push(sideBar);
-			var mainContent = document.getElementById('mainContent');
-			elements.push(mainContent);
-			var tallest = 0;
-			angular.forEach(elements, function(element) {
-				angular.element(element).css('height', '');
-				if (element) {
-					var thisHeight = angular.element(element)[0].scrollHeight;
-					if (thisHeight > tallest) {
-						tallest = thisHeight;
-					}
-				}
-			});
+		socket.on('NEW_EVENT', () => {
+			$rootScope.$broadcast('CHECK_EVENTS');
+		});
 
-			angular.forEach(elements, function(element) {
-				angular.element(element).css('height', tallest + 'px');
-			});
-		}
-
-		$scope.$on('UPDATE_HEIGHT', function() {
-			UPDATE_HEIGHT();
+		this.bindEvents($scope);
+		this.updateHeight();
+	}
+	bindEvents($scope) {
+		$scope.$on('UPDATE_HEIGHT', () => {
+			this.updateHeight();
 		});
 	}
-	updateFile() {
-		this.File.update(3, {
-				name: 'leandro.jpg'
-			})
-			.then(file => {
-				console.log(file);
-			})
-			.catch(error => {
-				console.log(error);
-			});
+	updateHeight() {
+		var elements = [];
+		var sideBar = document.getElementById('sideBar');
+		elements.push(sideBar);
+		var mainContent = document.getElementById('mainContent');
+		elements.push(mainContent);
+		var tallest = 0;
+		angular.forEach(elements, function(element) {
+			angular.element(element).css('height', '');
+			if (element) {
+				var thisHeight = angular.element(element)[0].scrollHeight;
+				if (thisHeight > tallest) {
+					tallest = thisHeight;
+				}
+			}
+		});
+
+		angular.forEach(elements, function(element) {
+			angular.element(element).css('height', tallest + 'px');
+		});
 	}
 }
 
